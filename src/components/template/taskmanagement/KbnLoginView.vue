bf47fe6f2cd52b07de44267363ecae3eab014e60
hajimekaneko <hajime.10600@gmail.com>
2022-06-04 23:28:29 +0900

    firebase 環境変数化

--- src/components/template/taskmanagement/KbnLoginView.vue
+++ src/components/template/taskmanagement/KbnLoginView.vue
@@ -17,7 +17,7 @@ import { useRouter } from "vue-router";
 import { initializeApp, getApps } from "firebase/app";
 // import firebase from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { firebaseConfig } from "@/key/firebaseconfig.js";
+// import { firebaseConfig } from "@/key/firebaseconfig.js";
 
 export default {
   name: "TaskManagement",
@@ -30,6 +30,15 @@ export default {
   setup() {
     const store = useStore();
     const router = useRouter();
+    const firebaseConfig = {
+      apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+      authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.VUE_APP_FIREBASE_APP_ID,
+      measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENTID,
+    };
     const authMethod = () => {
       if (!getApps().length) {
         initializeApp(firebaseConfig);
