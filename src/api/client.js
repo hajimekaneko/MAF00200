961c669fc8d1cad1340a1cfc596920006a33a968
hajimekaneko <hajime.10600@gmail.com>
2022-06-25 14:48:30 +0900

    heroku

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.VUE_APP_ROOT_API,
+  // baseURL: process.env.VUE_APP_ROOT_API,
+  baseURL: "https://mab00100.herokuapp.com/api/v1/",
   timeout: 50000,
   headers: {
     "Content-Type": "application/json",
